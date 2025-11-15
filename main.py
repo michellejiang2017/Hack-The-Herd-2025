3df6a7abfa22c17feafa3f3154bd7cd6abf3e4d7
Michelle Jiang <mhjiang@smith.edu>
2025-11-15 14:32:44 -0500

    added files

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 # Need to download Jinja and Flask beforehand
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename # security measure for file upload 
 from pathlib import Path
-import os 
-import sqlite3
